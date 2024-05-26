c404d8b80d717b90a9c56c071248641f2e53d9fd
Priyansh Kasera <priyanshkasera.ppkk@gmail.com>
2024-05-26 12:17:44 +0530

    fix auth option bug fix

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { NextApiRequest, NextApiResponse } from "next";
 import { pusherServer } from "@/app/libs/pusher";
+import { authOptions } from "@/app/libs/authOptions";
 
 export default async function handler(
   request: NextApiRequest,
